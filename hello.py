7bc58fc4c07c12c2608fbe6b209af4def1ff9820
Micah Evans <micah.evans@protonmail.com>
2022-02-10 09:51:12 -0700

    Updates

--- hello.py
+++ hello.py
@@ -2,15 +2,15 @@
 
 import random
 NUM_DIGITS = 3 #adjust for difficulty
-MAX_GUESSES = 10 # adjust for difficulty
+MAX_GUESSES = 6 # adjust for difficulty
 
 def main():
-    print('''Bagels, a deductive logic game. I  am thinking of a {}-digit number with no repeated digits. Try to guess what it is. Here are some clues:
+    print('''A deductive logic game. I  am thinking of a {}-digit number with no repeated digits. Try to guess what it is. Here are some clues:
 When I say: That means:
-Pico    One digit is correct but in the wrong position.
-Fermi   One digit is correct and in the right position.
+Banana    One digit is correct but in the wrong position.
+Foster   One digit is correct and in the right position.
 Bagels No digit is correct.
-For example, if the secret number was 248 and your guess was 843, the clues would be Fermi Pico.'''.format(NUM_DIGITS))
+For example, if the secret number was 248 and your guess was 843, the clues would be Foster Banana.'''.format(NUM_DIGITS))
 
     while True:  # Main game loop.
         # This stores the secret number the player needs to guess:
@@ -21,7 +21,7 @@ For example, if the secret number was 248 and your guess was 843, the clues woul
         numGuesses = 1
         while numGuesses <= MAX_GUESSES:
             guess = ''
-            # Keep looping until they enter a valid guess:
+            # Keep looping until enter a valid guess:
             while len(guess) != NUM_DIGITS or not guess.isdecimal():
                  print('Guess #{}: '.format(numGuesses))
                  guess = input('> ')
@@ -54,7 +54,7 @@ def getSecretNum():
     return secretNum
 
 def getClues(guess, secretNum):
-    """Returns a string with the pico, fermi, bagels clues for a guess
+    """Returns a string with the Banana, Foster, bagels clues for a guess
      and secret number pair."""
     if guess == secretNum:
         return 'You got it!'
@@ -64,10 +64,10 @@ def getClues(guess, secretNum):
     for i in range(len(guess)):
         if guess[i] == secretNum[i]:
             # A correct digit is in the correct place.
-            clues.append('Fermi')
+            clues.append('Foster')
         elif guess[i] in secretNum:
             # A correct digit is in the incorrect place.
-            clues.append('Pico')
+            clues.append('Banana')
         if len(clues) == 0:
             return 'Bagels'  # There are no correct digits at all.
         else:
